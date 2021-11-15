66207b31cf93f116c4364dec1e42d7ac12083cea
Hallvaeb <hallvard.bjorgen@gmail.com>
2021-11-15 15:46:08 +0100

    finalize UI (no styling)

--- paramServer.py
+++ paramServer.py
@@ -38,8 +38,17 @@ class MyHandler(BaseHTTPRequestHandler):
 			s.wfile.write(bytes('<input type="submit" value="submit"><br>', 'utf-8'))
 			s.wfile.write(bytes('</form></section>', 'utf-8'))
 
+		elif path.find("/reciept") != -1:
+			thankyou = """
+			<html lang="en"> <title> Gearbox </title></head>
+			<section><h1>Thank you! Your ordered has been registered! </h1>
+			... Disclaimer: Unfortunately, this is only a school project so estimated delivery is NEVER...<br>
+			"""
+			s.wfile.write(bytes(thankyou, 'utf-8'))
+			s.wfile.write(bytes('<a href="/"><button>Make another!</button></a>', 'utf-8'))
+			
 		else:
-			s.wfile.write(bytes('<html><head><title>Cool interface.</title></head>', 'utf-8'))
+			s.wfile.write(bytes('<html><head><title>Gearbox</title></head>', 'utf-8'))
 			s.wfile.write(bytes("<body><p>The path: " + path + "</p>", "utf-8"))
 			s.wfile.write(bytes('</body></html>', "utf-8"))
 			
@@ -62,18 +71,19 @@ class MyHandler(BaseHTTPRequestHandler):
 			# HTML title ++
 			s.wfile.write(bytes('<html lang="en"> <title> Gearbox </title></head>', 'utf-8'))
 			s.wfile.write(bytes('<title> Gearbox </title></head>', 'utf-8'))
-			s.wfile.write(bytes('<section><h1>GearBox - building [2/2]</h1><form action = "/review" method="post">', 'utf-8'))
-			s.wfile.write(bytes(nGears+' gears chosen! Choose the radiuses:<br><br>', 'utf-8'))
+			s.wfile.write(bytes('<section><h1>GearBox - building [2/2]</h1>', 'utf-8'))
+			s.wfile.write(bytes('<form action = "/review" method="post">', 'utf-8'))
+			s.wfile.write(bytes(nGears + ' gears chosen! Choose the radiuses:<br><br>', 'utf-8'))
 			
 			# Making a field for each gear radius
 			for i in range(int(nGears)): # nGears
-					s.wfile.write(bytes('<label for="gear"'+str(i)+'> Gear '+str(i+1)+":"+'</label>', 'utf-8'))
-					s.wfile.write(bytes('<input id = '+str(i)+' name = '"gear"+str(i)+' placeholder = "Radius [mm]" required> <br><br>', 'utf-8'))
-			# s.wfile.write(bytes('<input name = "nGears" value = str(nGears) </input>', 'utf-8'))
+					s.wfile.write(bytes('<label for="gear"' + str(i) + '> Gear ' + str(i+1) + ': </label>', 'utf-8'))
+					s.wfile.write(bytes('<input id = ' + str(i) + ' name = '"gear" + str(i) + ' placeholder = "Radius [mm]" required autofocus> <br><br>', 'utf-8'))
 		
 			# Making the buttons
 			s.wfile.write(bytes('<input type="submit" value="submit">', 'utf-8'))
 			s.wfile.write(bytes('</form></section>', 'utf-8'))
+			# Has to be outside form, pushes it down on the page.
 			s.wfile.write(bytes('<a href="/"><button>Go back</button></a>', 'utf-8'))
 
 		elif path.find("/review") != -1:
@@ -90,15 +100,53 @@ class MyHandler(BaseHTTPRequestHandler):
 				radius_list.append(int(pairs[i].split("=")[1]))
 
 			# Write up the page
-			out = """<html lang="en"> <title> Gearbox </title></head><title> Gearbox </title></head>
-					<section><h1>GearBox - review </h1><form action = "/accept" method="post">
-					nGears+' gears chosen. Radiuses are as follows:<br><br>"""
-			for i in range(radius_list):
-				out += ('Gear '+str(i)+': '+radius_list[i]+'\n')
+			out = """<html lang="en"> <title> Gearbox </title></head>
+					<section><h1>GearBox - review </h1>
+					""" + str(nGears) + ' gears chosen. Radiuses are as follows:<br><br>'
+			for i in range(nGears):
+				out += ('Gear nr.' + str(i) + ': ' + str(radius_list[i]) + ' [mm]<br>')
 			s.wfile.write(bytes(out, 'utf-8'))
+			s.wfile.write(bytes('<a href="/"><button>Go back</button></a>', 'utf-8'))
 			
-			
- 
+			# Skjema for bestilling
+			form = """<form action="/reciept">
+                <h2>We're ready to take your order!</h2>
+                <fieldset>
+                    <legend>Contact information</legend>
+                    <label for="company_name">Company name</label><br>
+                    <input type="text" name="company_name" placeholder="Company name" id="company_name"><br>
+                    <label for="contact_person">Contact person</label><br>
+                    <input type="text" name="contact_person" id="contact_person" placeholder="Contact person" pattern="^[a-zA-ZæøåÆØÅ -]+$"><br>
+                    <label for="phone">Phone</label><br>
+                    <input type="number" name="phone" placeholder="Phone" id="phone"><br>
+                    <label for="email">E-mail</label><br>
+                    <input class="last" type="email" name="email" placeholder="E-mail" id="email"><br>
+                </fieldset>
+                <fieldset>
+                    <legend>Gear specifications</legend>
+                    <label for= material" material</label><br>
+                    <select id= material" name= material">
+                        <option value="" disabled selected>Material</option>
+                        <option>Brass</option>
+                        <option>Steel</option>
+                        <option>Diamond</option>
+                        <option>Uncertain</option>
+                    </select><br>
+                    <label for="color">Color</label><br>
+                    <select id="color" name="color">
+                        <option value="" disabled selected>Color</option>
+                        <option>Default</option>
+                        <option>Have it painted</option>
+                        <option>Uncertain</option>
+                    </select><br>
+                    <label for="comments">Other comments</label><br>
+                    <textarea class="last" name="comments" placeholder="Other comments" id="comments" rows="4" cols="50"></textarea><br>
+                </fieldset>
+                <input type="submit" value="Order now!" id="submit">
+            	</form></section>"""
+			s.wfile.write(bytes(form, 'utf-8'))
+
+
 if __name__ == '__main__':
 	server_class = HTTPServer
 	httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
