2ab88d03cbfe71e892abaa016ff97d972147b9bd
Johanne Glende <johagl@ntnu.no>
2021-11-19 09:02:54 +0100

    oppdater FusekiRequest

--- nxRequest.py
+++ nxRequest.py
@@ -7,3 +7,24 @@ class nxRequest():
         # DFA or Python?
         print("make_gear")
         return "C:/Users/Eier/OneDrive/Studier/TMM4270/TMM4270_A3/Product_images/test.jpg"#run_in_nx(GearTrain.makeGearTrain(radius_list))
+
+
+
+    # #--------------Vi oppdaterer radiusene i py-filen som skal brukes for å lage GearBoxen i NX:--------------#
+
+	# # Reading the template file
+	# f = open("C:\\Users\\johagl\\auto\\templates\\gearBox_template.py", "r")
+	# txt = f.read()
+	# f.close()
+
+	# print("before:", txt)
+
+	# #Writing a new temporary file
+	# f = open("C:\\Users\\johagl\\auto\\templates\\gearBox_temporary"+str(orderNumber)+".py", "w")
+
+	# for i in range(\\BRUKERINPUT//): #ENDRES!!!!!!!!!
+	# 	#Replacement section, replace the old gear_radiuses with the ones from the use input
+	# 	txt = txt.replace(gear_radius_list[i], radius_list[i])
+	# 	#change it to the new radius 
+	# 	f.write(txt)
+	# f.close()
